74564910a4cf85546ecaf0f353daa65178dd59a6
Dan Johnson <dan@balancerockmedia.com>
2014-11-03 17:14:05 -0500

    edits

--- server/server.rb
+++ server/server.rb
@@ -1,6 +1,12 @@
 require 'sinatra'
 require 'json'
 
+users = [
+  {:id => 1, :name => 'Dan'},
+  {:id => 2, :name => 'Ella'},
+  {:id => 3, :name => 'Ava'}
+]
+
 before do
   content_type :json
   
@@ -16,12 +22,22 @@ error 500 do
 end
 
 # routes
+options '*' do
+  200
+end
+
 get '/users' do
-  [
-    {:id => 1, :name => 'Dan'},
-    {:id => 2, :name => 'Ella'},
-    {:id => 3, :name => 'Ava'}
-  ].to_json
+  users.to_json
+end
+
+post '/users' do
+  user = JSON.parse(request.env['rack.input'].read)
+  
+  users << user
+  
+  response['Location'] = "/users/#{user['id']}"
+  response.body = user.to_json
+  response.status = 201
 end
 
 get '/broken' do
